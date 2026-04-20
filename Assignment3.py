27a843222b81a86bd11dad30d45b614e7b96fba9
sumanruchi410-ui <sumanruchi410@gmail.com>
2026-04-20 23:45:05 +0530

    Create Assignment3.py

---
+++ Assignment3.py
@@ -0,0 +1,20 @@
+Tup1 = (10, 20, 30, 40, 50)
+print("Tuple :", Tup1)
+print("First Element :", Tup1[0])
+print("Last Element :", Tup1[-1])
+
+Tup2 = (1, 2, (3, 4, 5), 6)
+print("Nested Tuple :", Tup2)
+print("Access Nested Element :", Tup2[2][1])
+
+Tup3 = ('A', 'B')
+Repeated_tuple = Tup3 * 3
+print("Original Tuple :", Tup3)
+print("Repeated Tuple :", Repeated_tuple)
+
+Tup4 = (100, 200)
+Tup5 = (300, 400)
+Concatenated_tuple = Tup4 + Tup5
+print("First Tuple :", Tup4)
+print("Second Tuple :", Tup5)
+print("Concatenated Tuple:", Concatenated_tuple)
